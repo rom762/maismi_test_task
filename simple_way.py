99685d3b13dd1936cd1a1e68b611bc812c8fa38c
nickr <nickro@ya.ru>
2021-05-25 12:26:26 +0300

    request in own func

--- simple_way.py
+++ simple_way.py
@@ -12,36 +12,43 @@ def save_part(final_df, counter):
         print(f'I/O error({exp.errno}): {exp.strerror}')
 
 
+def get_requests(url):
+    try:
+        response = requests.get(url, headers={'x-requested-with': 'xmlhttprequest'})
+        response.raise_for_status()
+        if response.status_code == 200:
+            return response
+    except Exception as exp:
+        print(exp, exp.args)
+
+
 def main():
     page = 1
     final = pd.DataFrame()
 
     while True:
         url = f'https://xn--90adear.xn--p1ai/news/region?perPage=20&page={page}&region=65'
-        try:
-            response = requests.get(url, headers={'x-requested-with': 'xmlhttprequest'})
-            response.raise_for_status()
-        except Exception as exp:
-            print(exp, exp.args)
 
-        json_data = response.json()
-        items = response.json()['data']
+        response = get_requests(url)
+        if response:
+            json_data = response.json()
+            items = response.json()['data']
 
-        # convert parsed data from current page to pandas data-frame and put them to the final data-frame
-        df = pd.DataFrame.from_records(items)
-        final = pd.concat([final, df])
+            # convert parsed data from current page to pandas data-frame and put them to the final data-frame
+            df = pd.DataFrame.from_records(items)
+            final = pd.concat([final, df])
 
-        page = json_data['paginator']['page'] + 1
-        # each 10 page make back-up parsed data to the file
-        backup_counter = page % 10
-        if backup_counter == 0:
-            save_part(final, backup_counter)
+            page = json_data['paginator']['page'] + 1
+            # each 10 page make back-up parsed data to the file
+            backup_counter = page % 10
+            if backup_counter == 0:
+                save_part(final, backup_counter)
 
-        totals = json_data['paginator']['total']
-        per_page = json_data['paginator']['perPage']
+            totals = json_data['paginator']['total']
+            per_page = json_data['paginator']['perPage']
 
-        if totals // per_page + 1 < page:
-            break
+            if totals // per_page + 1 < page:
+                break
 
 
 if __name__ == '__main__':
